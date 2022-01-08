96c636c67971cce343af9012f87657cbe30edab8
Rudra-2009 <82301307+Rudra-2009@users.noreply.github.com>
2022-01-08 13:22:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function one() {
+    window.alert("QHD+ IPS Anti Glare Display Intel Core i5-11300H 11th Gen 14-inch(35.56 cms) Thin and Light Laptop (16GB/512GB SSD/Iris Xe Graphics/Win 10/Backlit KB/Fingerprint Sensor/1.4 Kg");
+}
+
+function two() {
+    window.alert(" (14-inch/35.97 cm, Apple M1 Pro chip with 8‑core CPU and 14‑core GPU, 16GB RAM, 512GB SSD) - Space Grey");
+}
+
+function three() {
+    window.alert("15.6 inches QHD 165Hz/3ms, AMD Ryzen 9  5900HX, RTX 3080 16GB Graphics, Gaming Laptop (32GB/2TB RAID 0 SSD/Office 2019/Windows 10 Home/Black/2.3 Kg), G533QS-HQ236TS");
+}
+
+function four() {
+    window.alert("(Stellar Black, 12GB RAM, 256GB  Storage)");
+}
+
+function five() {
+    window.alert("(Phantom Black, 16GB RAM, 512GB Storage)");
+}
+
+function six() {
+    window.alert("(512GB) - Graphite");
+}
+
+function seven() {
+    window.alert("A14 Bionic chip (10.9-inch/27.69 cm, Wi-Fi + Cellular, 256GB) - Sky Blue (4th Generation)");
+}
+
+function eight() {
+    window.alert("31.5 cm (12.4 inch) Super AMOLED 120 Hz Display, S-Pen in Box, Quad Speakers, 6 GB RAM 128 GB ROM, Wi-Fi+4G Tablet, (Mystic Silver)");
+}
+
+function nine() {
+    window.alert(" Apple M1 chip (12.9-inch/32.77 cm, Wi-Fi + Cellular, 2 TB) - Space Grey (5th Generation)")
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
